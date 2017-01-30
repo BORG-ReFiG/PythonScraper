fbe764c80124cc5efc2c5fbf0bdead29a964744b
Jane Kuznetsova <kuznetso@ualberta.ca>
2017-01-30 04:08:43 -0700

    Solved problem with encodings, added checks for fragments, added timer output

--- Search-Engine-and-Crawler/Crawler/crawlerExpand.py
+++ Search-Engine-and-Crawler/Crawler/crawlerExpand.py
@@ -1,4 +1,3 @@
-
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
@@ -6,6 +5,17 @@ import os.path
 import sys
 import tldextract
 import time
+import codecs
+import time
+try:
+    from os import scandir, walk
+except ImportError:
+    from scandir import scandir, walk
+
+
+#Pay attention to robots.txt
+
+#Add a user-agent and possibly other headers
 
 url = sys.argv[1]  # url to start from
 iterate = int(sys.argv[2])
@@ -13,19 +23,27 @@ depth_to_go = int(sys.argv[3])  # depth to go for
 directory = sys.argv[4]  # directory name
 if not url.startswith("http"):
     url = "http://" + url
-result = tldextract.extract(url)
-seed = result.domain
+seed = tldextract.extract(url).domain
+
+html = ""
+
+headers = requests.utils.default_headers()
+headers.update (
+    {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0',
+    }
+)
+
+print("Crawling through domain '" + seed + "'")
+
+#checks if the link provided is in the same domain as the seed
+def checkDomain(link):
+    link_domain = tldextract.extract(link)
+    return (link_domain.domain == seed)
 
 # max_pages is the number of pages to crawl
-
-def checkDomain(link, seed):
-    same = False
-    link_domain = tldextract.extract(link)
-    if link_domain.domain == seed:
-        same = True
-    return same
-
 def trade_spider(max_pages):  # function(maximum number of pages to call variable)
+    global html
     page = 1
 
     urls = [url]
@@ -35,17 +53,21 @@ def trade_spider(max_pages):  # function(maximum number of pages to call variabl
         os.mkdir(directory)  # if it doesnt then make it
     os.chdir(directory)  # then change directory to that folder
 
-    dsize = 0  # makes the amount of depth already crawled 0
+    dsize = 0  # makes the depth already crawled 0
     depth = [dsize]
     # this checks if pg < max pg, the depth is < depth_to_go, and that urls are still available
     while page <= max_pages and depth_to_go >= dsize and len(urls) > 0:
         try:
             try:
-                source_code = requests.get(urls[0])  # variable = requests.get(url)
-                html = source_code.text  # get source code of page
-                soup = BeautifulSoup(html, 'html.parser')  # variable to call beautifulsoup(variable of the source code)
+                while (bool(html) != True):
+                    source_code = requests.get(urls[0])  # variable = requests.get(url)
+                    html = source_code.text  # get source code of page
+                    if html:
+                        soup = BeautifulSoup(html, 'html.parser')  # variable to call beautifulsoup(variable of the source code)
+                    else:
+                        print('Request returned empty html')
             except:
-                print('help ' + urls[0])
+                print('Couldn\'t request an html response ' + urls[0])
 
             try:
                 name = soup.title.string  # removes all the uncessary things from title
@@ -54,36 +76,36 @@ def trade_spider(max_pages):  # function(maximum number of pages to call variabl
                 name = name.replace("\t", "")
                 name = name.replace("|", "")
                 name = name.replace(":", "")
+                name = name.replace("?", "")
                 name = name.strip(' ')
-                print('created name ' + name)
+                print('Created name ' + name)
             except:
                 name = "no title " + str(title_number)  # if not title provided give a no title with number title
                 title_number += 1
-                print('failed creating name')
+                print('Failed to create a name, using \'' + name + '\' instead')
 
             num = 1
             name = "{0}.txt".format(name)  # adds the .txt to the end of the name
-            print('got this far')
             try:
                 if not os.path.isfile(name):  # if the file doesn't exist makes it
-                    print('here I am')
-                    fo = open(name, "w")
+                    print('Creating a new file...')
+                    fo = codecs.open(name, "w", "utf-8-sig")
 
                     fo.write('<page_url href=\"' + urls[0] + '\"></page_url>\n' + html)
                     fo.close()
 
                     size = os.stat(name)
                     size = size.st_size
-                    print('created file')
+                    print('Created a new file.')
 
-                    if size2 == 0:
+                    if size == 0:
                         os.remove(name)
-                        print('removed file')
+                        print('Removed file')
                 else:  # if it does exists checks if it's the same file
                     new_name = name[:name.find(".")]
-                    new_name = str(num) + ".txt"
-                    fo = open(new_name, "w")
-                    fo.write(html)
+                    new_name += "_" + str(num) + ".txt"
+                    fo = codecs.open(new_name, "w", "utf-8-sig")
+                    fo.write('<page_url href=\"' + urls[0] + '\"></page_url>\n' + html)
                     fo.close()
 
                     size = os.stat(name)
@@ -99,10 +121,10 @@ def trade_spider(max_pages):  # function(maximum number of pages to call variabl
                         os.remove(new_name)
                 print(urls[0])
                 for link in soup.findAll('a', href=True): #this is new, it makes sure to only collect from the site we want
-                    link['href'] = urllib.parse.urljoin(urls[0], link['href']) # checks the domain name
-                    same = checkDomain(link['href'], seed)
-                    if same == True:
-                        if link['href'] not in visited:  # if the link is not in visited then it appends it to urls and visited
+                    new_link = urllib.parse.urldefrag(link['href'])[0].rstrip('/')
+                    new_link = urllib.parse.urljoin(urls[0], new_link)
+                    if checkDomain(new_link):
+                        if new_link not in visited:  # if the link is not in visited then it appends it to urls and visited
                             if '.pdf' not in link['href'] and '.jpg' not in link['href']:#makes sure no jpg or pdfs pass
                                 urls.append(link['href'])
                                 visited.append(link['href'])
@@ -114,6 +136,7 @@ def trade_spider(max_pages):  # function(maximum number of pages to call variabl
         print("depth:", dsize)
         print("iterations:", page, "pages")
         urls.pop(0)
+        html = ''
 
         if page >= depth[dsize]:
             depth.append(len(visited))
@@ -131,7 +154,7 @@ def trade_spider(max_pages):  # function(maximum number of pages to call variabl
 def get_tree_size(path):
     """Return total size of files in given path and subdirs."""
     total = 0
-    for entry in os.scandir(path):
+    for entry in scandir(path):
         if entry.is_dir(follow_symlinks=False):
             total += get_tree_size(entry.path)
         else:
@@ -139,4 +162,10 @@ def get_tree_size(path):
     return total
 
 
+start_time = time.time()
 trade_spider(iterate)
+end_time = time.time()
+
+print (bool(html))
+
+print("Overall time: " + str((end_time - start_time)))
